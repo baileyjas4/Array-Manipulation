ef1677d520e28aeeb116c8620c8b42f9707419b3
baileyjas4 <baileyjasmine4@icloud.com>
2025-11-07 16:19:26 -0500

    task2

---
+++ script.js
@@ -0,0 +1,15 @@
+let shoppingList = ["Eggs", "Mik", "Bread"];
+
+function addItem(item) {
+    shoppingList.push(item);
+}
+
+function removeLastItem() {
+    shoppingList.pop(item);
+}
+
+
+function displayList() {
+    console.log(shoppingList);
+}
+
