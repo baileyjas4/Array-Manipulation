4f27820c2fa696b4981b7c926d1bc12293953467
baileyjas4 <baileyjasmine4@icloud.com>
2025-11-09 15:22:12 -0500

    update

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
-let shoppingList = ["Eggs", "Mik", "Bread"];
-
+let shoppingList = [];
+// Adds new items to the list
 function addItem(item) {
-    shoppingList.push(item);
+
+    if (!shoppingList.includes(item)){ //checking for duplicates in the list if there is none we add the item if there is we notify that the item is already there
+        shoppingList.push(item);
+        return item + ' is added'
+    } else {
+        return item + ' is already in the list.'
+    }
+
 }
+console.log(addItem('bread'));
+console.log(addItem('cheese'));
+console.log(addItem('bread'));
+
+console.log(shoppingList)
+
 
 function removeLastItem() {
     shoppingList.pop(item);
 }
 
 
-function displayList() {
-    console.log(shoppingList);
-}
+// function displayList() {
+//     console.log(shoppingList);
+// }
+
+
+
 
